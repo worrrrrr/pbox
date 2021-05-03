dea1f05ce7ed71e3224b19c821792078146df0d2
worrrrrr <worrrrrr@gmail.com>
2021-05-04 00:58:02 +0700

    supabs

---
+++ src/routes/user/update.svelte
@@ -0,0 +1,21 @@
+<script>
+import supabase from "$lib/db";
+
+
+
+    async function update(){
+        const { user, error } = await supabase.auth.update({
+            email: "new@email.com",
+            password: "new-password",
+            data: { hello: 'world' }
+        })
+        
+    }
+      // your script goes here
+  </script>
+  
+  <style>
+      /* your styles go here */
+  </style>
+  
+  <!-- markup (zero or more items) goes here -->
